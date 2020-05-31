5e068fde1f22c0c1ec3f08745536e1b7c79195a9
Rene Dellefont <renedellefont@gmail.com>
2020-05-31 13:11:21 -0400

    Init site

---
+++ src/components/Icon.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let width = 16
+  export let height = 16
+  export let name = ''
+  export let color = 'currentColor'
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  {width}
+  {height}
+  viewBox="0 0 20 20"
+  aria-labelledby={name}
+  role="presentation"
+>
+  <title id={name} lang="en">{`${name} icon`}</title>
+  <g fill={color}>
+    <slot />
+  </g>
+</svg>
+
+<style>
+  svg {
+    display: inline-block;
+    vertical-align: middle;
+  }
+</style>
